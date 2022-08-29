f9a6f9818e2bd261fdf653a9bbe6a41867633d77
Tommaso Cammelli <tomm.camm@protonmail.ch>
2022-08-29 18:59:42 +0200

    initial work on the rust side

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,14 +3,44 @@
     windows_subsystem = "windows"
 )]
 
+use std::error::Error;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct Record {
+    id: String,
+    value: String
+}
+
+#[allow(non_snake_case)]
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn add_record(recordName: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", recordName)
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![add_record])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+// Updates the database using the current entry 
+fn updateDB (key: String, value: String) -> Result<(), Box<dyn Error>>{
+    let mut rdr = csv::ReaderBuilder::new()
+        .has_headers(false)
+        .from_path("/tmp/tpwman.db")?;
+    
+
+    for result in rdr.deserialize() {
+        let record: Record = result?;
+        println!("{:?}", record); // temporary print all records to see if it works
+    }
+
+    Ok(())
+}
+
+// Remove a record from the database
+fn removeRecordDB (key: String) -> Result<(), Box<dyn Error>>{
+    Ok(())
 }
